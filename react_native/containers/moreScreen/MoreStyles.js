e9bd6ce3c7abd061a7fb0b9b51a78539b99db4e2
naveens <naveen.srivastava@thetrainline.com>
2020-10-03 02:36:18 +0530

    Changed Style for Language On More Screen

--- react_native/containers/moreScreen/MoreStyles.js
+++ react_native/containers/moreScreen/MoreStyles.js
@@ -2,103 +2,109 @@ import $primary from '../../settings/styles/DefaultPrimarySettings';
 
 export default {
 
-    moreContainer:{
+    moreContainer: {
         //backgroundColor: $primary.white,
-        height:'100%'
+        height: '100%',
+        flex:1
     },
 
-    headerSection:{
+    headerSection: {
         backgroundColor: $primary.white,
-        paddingVertical:50,
-        paddingHorizontal:15,
+        paddingVertical: 50,
+        paddingHorizontal: 15,
         flexDirection: 'row',
     },
-    languageBodySection:{
+    languageBodySection: {
         flexDirection: 'row',
-        borderBottomWidth:1,
-        paddingTop:20,
-        borderColor:$primary.grayColors._900
+        borderBottomWidth: 1,
+        paddingTop: 20,
+        borderColor: $primary.grayColors._900,
+        ios: {
+            paddingTop: 30,
+            paddingBottom: 30
+        }
+
     },
-    VersionBodySection:{
+    VersionBodySection: {
         flexDirection: 'row',
-        borderBottomWidth:1,
-        paddingVertical:25,
-        borderColor:$primary.grayColors._900
+        borderBottomWidth: 1,
+        paddingVertical: 25,
+        borderColor: $primary.grayColors._900
     },
-    bodySectionValueConatiner:{
+    bodySectionValueConatiner: {
         // textAlign:'center',
-        width:'50%',
+        width: '50%',
         // paddingRight:30
+        
     },
-    bodySectionValue:{
-        textAlign:'right',
-        paddingRight:20,
+    bodySectionValue: {
+        textAlign: 'right',
+        paddingRight: 20,
         // marginRight:10,
         fontSize: 16
     },
-    arrowIcon:{
-        textAlign:'right',
-        top :3,
-        paddingRight:15,
-
-        // android:{ paddingTop: 0 },
-        // ios:{ paddingTop: 0 }
+    arrowIcon: {
+        textAlign: 'right',
+        top: 3,
+        paddingRight: 15,
+        ios:{ paddingRight: 8 }
     },
 
-    bodySectionKey:{
+    bodySectionKey: {
         flexDirection: 'row',
-        width:'50%',
+        width: '50%',
         paddingLeft: 10,
 
     },
-    nameSection:{
-        paddingLeft:15,
-        paddingTop:12,
+    nameSection: {
+        paddingLeft: 15,
+        paddingTop: 12,
     },
-    headerNameText:{
-        fontSize:18,
+    headerNameText: {
+        fontSize: 18,
         flexWrap: 'wrap'
     },
-    headerEmailText:{
-        fontSize:16,
+    headerEmailText: {
+        fontSize: 16,
         flexWrap: 'wrap',
-        color:$primary.grayColors._800
+        color: $primary.grayColors._800
     },
-    languageAndVersion:{
+    languageAndVersion: {
         marginTop: 20,
         backgroundColor: $primary.white,
         paddingBottom: 100,
     },
 
-    logoutSection:{
-        marginTop:10,
-        paddingVertical:20,
+    logoutSection: {
+        marginTop: 10,
+        paddingVertical: 20,
         backgroundColor: $primary.white,
     },
 
-    logoutText:{
-        fontSize:16,
-        textAlign:'center',
+    logoutText: {
+        fontSize: 16,
+        textAlign: 'center',
     },
 
-    icon_head:{
-        width:70,
+    icon_head: {
+        width: 70,
         resizeMode: 'contain',
         height: 70,
     },
-    body_icon:{
+    body_icon: {
         resizeMode: 'contain',
-        width:22,
+        width: 22,
         height: 22,
     },
-    bodyIconTextContainer:{
+    bodyIconTextContainer: {
         paddingLeft: 10,
-
     },
-    bodyIconText:{
+    bodyIconText: {
         fontSize: 16,
     },
-    pickerStyle:{
-        paddingLeft: 70
+    pickerStyle: {
+        paddingLeft: 70,
+        ios:{ paddingRight: 8 }
+
     }
 }
